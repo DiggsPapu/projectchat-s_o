a2798b6321d7d3e6f698fee512a003da63d4a248
DiggsPapu <alo20172@uvg.edu.gt>
2023-04-09 01:28:13 -0600

    Added the correct use of port an ip

--- Example/clientex.cpp
+++ Example/clientex.cpp
@@ -10,32 +10,34 @@
 
 int main(int argc, char const* argv[])
 {
+	printf("%s\n",argv[1]);
 	int status, valread, client_fd;
 	struct sockaddr_in serv_addr;
 	char* hello = "Hello from client";
 	char buffer[1024] = { 0 };
+	if (argc != 4)
+	{
+		fprintf(stderr, "Client use: client <username> <server_ip> <server_port>\n");
+		exit(1);
+	}
 	if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
 		printf("\n Socket creation error \n");
 		return -1;
 	}
-    int port;
-    port = atoi(argv[1]);
+    int port = atoi(argv[3]);
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(port);
 
 	// Convert IPv4 and IPv6 addresses from text to binary
 	// form
-	if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)
+	if (inet_pton(AF_INET, argv[2], &serv_addr.sin_addr)
 		<= 0) {
 		printf(
 			"\nInvalid address/ Address not supported \n");
 		return -1;
 	}
 
-	if ((status
-		= connect(client_fd, (struct sockaddr*)&serv_addr,
-				sizeof(serv_addr)))
-		< 0) {
+	if ((status = connect(client_fd, (struct sockaddr*)&serv_addr,sizeof(serv_addr)))< 0) {
 		printf("\nConnection Failed \n");
 		return -1;
 	}
--- Example/serverex.cpp
+++ Example/serverex.cpp
@@ -8,6 +8,8 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <semaphore.h>
 #include "project.pb.h"
 
 int main(int argc, char const* argv[])
@@ -18,11 +20,14 @@ int main(int argc, char const* argv[])
         fprintf(stderr, "Use: server <puertodelservidor>\n");
         return 1;
     }
+	// Assigning port
+	int port = atoi(argv[1]);
     /**
      * CREATING THE SOCKET AND CONNFIGURING IT
     */
 	int server_fd, new_socket, valread;
-	struct sockaddr_in address;
+	struct sockaddr_in address, new_connection;
+    socklen_t new_conn_size;
 	int opt = 1;
 	int addrlen = sizeof(address);
 	char buffer[1024] = { 0 };
@@ -43,14 +48,14 @@ int main(int argc, char const* argv[])
     // // Aws ip public should be defined here:
     // serv_addr.sin_addr.s_addr = inet_addr("dirección IP pública de la instancia de AWS");
 	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons(atoi(argv[1]));
+	address.sin_port = htons(port);
 
 	// Forcefully attaching socket to the port entered
 	if (bind(server_fd, (struct sockaddr*)&address,sizeof(address))< 0) {
 		perror("Server: bind failed IP socket->port");
 		exit(EXIT_FAILURE);
 	}
-	if (listen(server_fd, 3) < 0) {
+	if (listen(server_fd, 5) < 0) { //5 first connection before refused.
 		perror("listen");
 		exit(EXIT_FAILURE);
 	}
@@ -58,6 +63,19 @@ int main(int argc, char const* argv[])
 		perror("accept");
 		exit(EXIT_FAILURE);
 	}
+	printf("Listening on port %d\n", port);
+	pthread_t tid[5];
+	int i = 0;
+	while (1)
+	{
+		new_conn_size = sizeof new_connection;
+		 if (accept(server_fd, (struct sockaddr *)&new_connection, &new_conn_size)==-1)
+		 {
+			perror("Error in accept");
+            continue;
+		 }
+	}
+	
 	valread = read(new_socket, buffer, 1024);
 	printf("%s\n", buffer);
 	send(new_socket, hello, strlen(hello), 0);
